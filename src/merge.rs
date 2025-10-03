b7991f22e6979a5ba1d1952e6abfa936dd85ff86
mikai <dreamfever2017@yahoo.com>
2025-10-03 12:20:20 +0800

    fix: Merge branch 'master' into stack

--- src/merge.rs
+++ src/merge.rs
@@ -2,6 +2,7 @@ pub(crate) mod add_assign;
 pub(crate) mod array;
 pub(crate) mod concat;
 pub(crate) mod delay_replacement;
+pub(crate) mod memo;
 pub(crate) mod object;
 pub(crate) mod path;
 pub(crate) mod substitution;
